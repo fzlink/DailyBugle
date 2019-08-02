060d2148fcacfbdf20d191791df398b3b38e6a2a
Fazlı <36913092+fzlink@users.noreply.github.com>
2019-08-02 15:21:48 +0300

    Project Initialized
    Added Content

---
+++ DailyBugle/DailyBugle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DailyBugle.MvcApplication" Language="C#" %>
---
+++ DailyBugle/DailyBugle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DailyBugle.Startup))]
+namespace DailyBugle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
